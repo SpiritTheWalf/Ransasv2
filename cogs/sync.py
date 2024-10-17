7877293905d1c56d46dc8183629e51d0ae91573c
SpiritTheWalf <spirit@spiritthewalf.co.uk>
2024-10-17 13:40:41 +0100

    Linting changes

--- cogs/sync.py
+++ cogs/sync.py
@@ -1,9 +1,10 @@
-import discord
+"""Cog for syncing commands with Discord"""
 from discord.ext import commands
-from discord import app_commands
+
 
 
 class Owner(commands.Cog):
+    """Cog for syncing commands with Discord"""
     def __init__(self, bot):
         self.bot = bot
 
@@ -11,9 +12,11 @@ class Owner(commands.Cog):
     @commands.command(name="sync", hidden=True)
     @commands.is_owner()
     async def sync(self, ctx):
+        """Sync commands with Discord"""
         await ctx.bot.tree.sync()
         await ctx.send("Commands synced, you will need to reload Discord to see them")
         await ctx.message.delete()
 
 async def setup(bot):
+    """Add the Owner cog to the bot"""
     await bot.add_cog(Owner(bot))
